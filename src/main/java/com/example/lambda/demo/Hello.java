9e47f2e6f1d5678e9c6a3680f93990a81f90bad5
Patanjali Chinigiri <Patanjali@Patanjali-MacBook-Pro.local>
2019-01-07 02:05:26 -0500

    lambda hello

---
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.example.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+        context.getLogger().log("context: " + context.getAwsRequestId());
+        return "Hello " + input;
+    }
+
+}
